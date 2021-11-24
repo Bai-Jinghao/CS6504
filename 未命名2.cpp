e3e2b81e849581cdf172c93a652ade6b904102df
Bai-Jinghao <64741207+Bai-Jinghao@users.noreply.github.com>
2021-11-24 19:48:52 +0000

    Add files via upload

---
+++ 未命名2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int	x, y;
+	
+	 
+	printf("Please input x =");
+	scanf("%d", &x);
+	
+	printf("Please input y =");
+	scanf("%d", &y);
+		
+	printf("x + y  = %d\n",x+y);
+} 
